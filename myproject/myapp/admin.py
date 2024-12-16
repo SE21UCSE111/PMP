33721faf3b9a7d67e1bd127fffe5811e6d236234
SE21UCSE111 <160005432+SE21UCSE111@users.noreply.github.com>
2024-12-17 01:51:45 +0530

    Delete myproject directory

--- myproject/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-# admin.py
-from django.contrib import admin
-from .models import ProfessorEmail, Project, Profile, Team
-
-admin.site.register(ProfessorEmail)
-admin.site.register(Project)
-admin.site.register(Profile)
-admin.site.register(Team)
