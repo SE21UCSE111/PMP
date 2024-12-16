1c37d1fa821322c19806da15f31f02c08f55bf91
SE21UCSE111 <160005432+SE21UCSE111@users.noreply.github.com>
2024-12-17 02:31:08 +0530

    Delete myproject2 directory

--- myproject2/myapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Project, CustomUser  # Ensure CustomUser is imported
-
-class ProjectSerializer(serializers.ModelSerializer):
-    created_by = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
-
-    class Meta:
-        model = Project
-        fields = ['id', 'name', 'description', 'created_by']
